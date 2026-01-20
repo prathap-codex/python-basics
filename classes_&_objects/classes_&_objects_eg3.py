5118a2fbb9407fcd36ed3467400adbee450d5557
prathap-codex <prathapkrishnaveni12@gmail.com>
2026-01-20 15:52:41 +0530

    added classes and objects example program

---
+++ classes_&_objects/classes_&_objects_eg3.py
@@ -0,0 +1,21 @@
+class student:
+    def __init__(self):
+        self.name = "ajay"
+        self.regno = "5745"
+
+    def display(self):
+        print("name: ", self.name)
+        print("regno: ", self.regno)
+
+
+s1 = student()
+s2 = student()
+
+s1.name = "gowri"
+s1.regno = "1"
+
+s2.name = "prathap"
+s2.regno = "2"
+
+s1.display()
+s2.display()
